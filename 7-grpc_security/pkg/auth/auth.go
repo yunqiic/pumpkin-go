2fff58bfc272d00a66b9ba526af8aed71770a2b7
zhangchunsheng <zhangchunsheng423@gmail.com>
2021-10-22 15:47:53 +0800

    4. add tls grpc test

--- 7-grpc_security/pkg/auth/auth.go
+++ 7-grpc_security/pkg/auth/auth.go
@@ -1,1 +1,18 @@
 package auth
+
+import (
+	"context"
+)
+
+type Token struct {
+	AppID     string
+	AppSecret string
+}
+
+func (t *Token) GetRequestMetadata(ctx context.Context, uri ...string) (map[string]string, error) {
+	return map[string]string{"app_id": t.AppID, "app_secret": t.AppSecret}, nil
+}
+
+func (t *Token) RequireTransportSecurity() bool {
+	return true
+}
