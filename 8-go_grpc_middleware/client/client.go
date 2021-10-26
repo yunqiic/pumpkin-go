48119a21cab75ec271bf34d5b03a548e2c88e6e2
zhangchunsheng <zhangchunsheng423@gmail.com>
2021-10-26 10:46:43 +0800

    1. add tls grpc test

---
+++ 8-go_grpc_middleware/client/client.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+
+	"pumpkin-go/8-go_grpc_middleware/client/auth"
+	pb "pumpkin-go/8-go_grpc_middleware/proto"
+)
+
+const Address string = ":8000"
+
+var grpcClient pb.SimpleClient
+
+func main() {
+	credential, err := credentials.NewClientTLSFromFile("../pkg/tls/test.crt", "*.yunqiic.com")
+	if err != nil {
+		log.Fatalf("Failed to create TLS credentials: %v", err)
+	}
+	token := auth.Token{
+		Value: "bearer grpc.auth.token",
+	}
+
+	conn, err := grpc.Dial(Address, grpc.WithTransportCredentials(credential), grpc.WithPerRPCCredentials(&token))
+	if err != nil {
+		log.Fatalf("net.Connect err: %v", err)
+	}
+	defer conn.Close()
+
+	grpcClient = pb.NewSimpleClient(conn)
+	route()
+}
+
+func route() {
+	req := pb.SimpleRequest{
+		Data: "grpc",
+	}
+
+	res, err := grpcClient.Route(context.Background(), &req)
+	if err != nil {
+		log.Fatalf("Call Route err: %v", err)
+	}
+	log.Println(res)
+}
