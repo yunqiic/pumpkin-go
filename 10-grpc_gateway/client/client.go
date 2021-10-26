45dfd6f12d6031458b61e839a7c600abbdc4685e
zhangchunsheng <zhangchunsheng423@gmail.com>
2021-10-26 15:38:37 +0800

    1. add tls grpc test

--- 10-grpc_gateway/client/client.go
+++ 10-grpc_gateway/client/client.go
@@ -1,1 +1,48 @@
-package client
+package main
+
+import (
+	"context"
+	"log"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+
+	"pumpkin-go/10-grpc_gateway/client/auth"
+	pb "pumpkin-go/10-grpc_gateway/proto"
+)
+
+const Address string = ":8000"
+
+var grpcClient pb.SimpleClient
+
+func main() {
+	credential, err := credentials.NewClientTLSFromFile("../tls/test.crt", "*.yunqiic.com")
+	if err != nil {
+		log.Fatalf("Failed to create TLS credentials: %v", err)
+	}
+	token := auth.Token{
+		Value: "bearer grpc.auth.token",
+	}
+
+	conn, err := grpc.Dial(Address, grpc.WithTransportCredentials(credential), grpc.WithPerRPCCredentials(&token))
+	if err != nil {
+		log.Fatalf("net.Connect err: %v", err)
+	}
+	defer conn.Close()
+
+	grpcClient = pb.NewSimpleClient(conn)
+	route()
+}
+
+func route() {
+	req := pb.InnerMessage{
+		SomeInteger: 99,
+		SomeFloat:   1,
+	}
+
+	res, err := grpcClient.Route(context.Background(), &req)
+	if err != nil {
+		log.Fatalf("Call Route err: %v", err)
+	}
+	log.Println(res)
+}
--- 10-grpc_gateway/server/server.go
+++ 10-grpc_gateway/server/server.go
@@ -1,1 +1,65 @@
-package server
+package main
+
+import (
+	"context"
+	"log"
+	"net"
+
+	grpc_middleware "github.com/grpc-ecosystem/go-grpc-middleware"
+	grpc_auth "github.com/grpc-ecosystem/go-grpc-middleware/auth"
+	grpc_zap "github.com/grpc-ecosystem/go-grpc-middleware/logging/zap"
+	grpc_recovery "github.com/grpc-ecosystem/go-grpc-middleware/recovery"
+	grpc_validator "github.com/grpc-ecosystem/go-grpc-middleware/validator"
+	"google.golang.org/grpc"
+
+	pb "pumpkin-go/10-grpc_gateway/proto"
+	"pumpkin-go/10-grpc_gateway/server/middleware/auth"
+	"pumpkin-go/10-grpc_gateway/server/middleware/cred"
+	"pumpkin-go/10-grpc_gateway/server/middleware/recovery"
+	"pumpkin-go/10-grpc_gateway/server/middleware/zap"
+)
+
+type SimpleService struct {
+	pb.UnimplementedSimpleServer
+}
+
+func (s *SimpleService) Route(ctx context.Context, req *pb.InnerMessage) (*pb.OuterMessage, error) {
+	res := pb.OuterMessage{
+		ImportantString: "hello grpc validator",
+		Inner:           req,
+	}
+	return &res, nil
+}
+
+const (
+	Address string = ":8000"
+	Network string = "tcp"
+)
+
+func main() {
+	listener, err := net.Listen(Network, Address)
+	if err != nil {
+		log.Fatalf("net.Lister err: %v", err)
+	}
+
+	grpcServer := grpc.NewServer(cred.TLSInterceptor(),
+		grpc.StreamInterceptor(grpc_middleware.ChainStreamServer(
+			grpc_validator.StreamServerInterceptor(),
+			grpc_zap.StreamServerInterceptor(zap.ZapInterceptor()),
+			grpc_auth.StreamServerInterceptor(auth.AuthInterceptor),
+			grpc_recovery.StreamServerInterceptor(recovery.RecoveryInterceptor()),
+		)),
+		grpc.UnaryInterceptor(grpc_middleware.ChainUnaryServer(
+			grpc_validator.UnaryServerInterceptor(),
+			grpc_zap.UnaryServerInterceptor(zap.ZapInterceptor()),
+			grpc_auth.UnaryServerInterceptor(auth.AuthInterceptor),
+			grpc_recovery.UnaryServerInterceptor(recovery.RecoveryInterceptor()),
+		)),
+	)
+	pb.RegisterSimpleServer(grpcServer, &SimpleService{})
+	log.Println(Address + " net.Listing with TLS and token...")
+	err = grpcServer.Serve(listener)
+	if err != nil {
+		log.Fatalf("grpcServer.Serve err: %v", err)
+	}
+}
