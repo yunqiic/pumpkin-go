c2ba7053208183015964c61051034f5fc1d951c6
zhangchunsheng <zhangchunsheng423@gmail.com>
2021-10-26 11:23:35 +0800

    1. add tls grpc test

--- 8-go_grpc_middleware/server/server.go
+++ 8-go_grpc_middleware/server/server.go
@@ -1,1 +1,62 @@
-package server
+package main
+
+import (
+	"context"
+	"log"
+	"net"
+
+	grpc_middleware "github.com/grpc-ecosystem/go-grpc-middleware"
+	grpc_auth "github.com/grpc-ecosystem/go-grpc-middleware/auth"
+	grpc_zap "github.com/grpc-ecosystem/go-grpc-middleware/logging/zap"
+	grpc_recovery "github.com/grpc-ecosystem/go-grpc-middleware/recovery"
+	"google.golang.org/grpc"
+
+	pb "pumpkin-go/8-go_grpc_middleware/proto"
+	"pumpkin-go/8-go_grpc_middleware/server/middleware/auth"
+	"pumpkin-go/8-go_grpc_middleware/server/middleware/cred"
+	"pumpkin-go/8-go_grpc_middleware/server/middleware/recovery"
+	"pumpkin-go/8-go_grpc_middleware/server/middleware/zap"
+)
+
+type SimpleService struct {
+	pb.UnimplementedSimpleServer
+}
+
+func (s *SimpleService) Route(ctx context.Context, req *pb.SimpleRequest) (*pb.SimpleResponse, error) {
+	res := pb.SimpleResponse{
+		Code:  200,
+		Value: "hello " + req.Data,
+	}
+	return &res, nil
+}
+
+const (
+	Address string = ":8000"
+	Network string = "tcp"
+)
+
+func main() {
+	listener, err := net.Listen(Network, Address)
+	if err != nil {
+		log.Fatalf("net.Lister err: %v", err)
+	}
+
+	grpcServer := grpc.NewServer(cred.TLSInterceptor(),
+		grpc.StreamInterceptor(grpc_middleware.ChainStreamServer(
+			grpc_zap.StreamServerInterceptor(zap.ZapInterceptor()),
+			grpc_auth.StreamServerInterceptor(auth.AuthInterceptor),
+			grpc_recovery.StreamServerInterceptor(recovery.RecoveryInterceptor()),
+		)),
+		grpc.UnaryInterceptor(grpc_middleware.ChainUnaryServer(
+			grpc_zap.UnaryServerInterceptor(zap.ZapInterceptor()),
+			grpc_auth.UnaryServerInterceptor(auth.AuthInterceptor),
+			grpc_recovery.UnaryServerInterceptor(recovery.RecoveryInterceptor()),
+		)),
+	)
+	pb.RegisterSimpleServer(grpcServer, &SimpleService{})
+	log.Println(Address + " net.Listing with TLS and token...")
+	err = grpcServer.Serve(listener)
+	if err != nil {
+		log.Fatalf("grpcServer.Serve err: %v", err)
+	}
+}
