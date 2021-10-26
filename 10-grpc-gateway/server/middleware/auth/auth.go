f283a24477a710334da0d4b60d63d6b533679baf
zhangchunsheng <zhangchunsheng423@gmail.com>
2021-10-26 13:59:16 +0800

    1. add tls grpc test

---
+++ 10-grpc-gateway/server/middleware/auth/auth.go
@@ -0,0 +1,42 @@
+package auth
+
+import (
+	"context"
+	"errors"
+
+	grpc_auth "github.com/grpc-ecosystem/go-grpc-middleware/auth"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+)
+
+type TokenInfo struct {
+	ID    string
+	Roles []string
+}
+
+func AuthInterceptor(ctx context.Context) (context.Context, error) {
+	token, err := grpc_auth.AuthFromMD(ctx, "bearer")
+	if err != nil {
+		return nil, err
+	}
+	tokenInfo, err := parseToken(token)
+	if err != nil {
+		return nil, grpc.Errorf(codes.Unauthenticated, " %v", err)
+	}
+	newCtx := context.WithValue(ctx, tokenInfo.ID, tokenInfo)
+	return newCtx, nil
+}
+
+func parseToken(token string) (TokenInfo, error) {
+	var tokenInfo TokenInfo
+	if token == "grpc.auth.token" {
+		tokenInfo.ID = "1"
+		tokenInfo.Roles = []string{"admin"}
+		return tokenInfo, nil
+	}
+	return tokenInfo, errors.New("Token err: bearer " + token)
+}
+
+func userClaimFromToken(tokenInfo TokenInfo) string {
+	return tokenInfo.ID
+}
